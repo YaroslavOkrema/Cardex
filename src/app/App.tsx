5852acd101deebcda9cfc136137fbeac436f01dd
YaroslavOkrema <yaroslav.okrema@gmail.com>
2025-04-22 23:17:43 +0300

    chore: init project, tailwind, prettier, shadcn

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button.tsx'
+
+function App() {
+  return (
+    <>
+      <div className="text-3xl font-bold underline">Car</div>
+      <Button type="button">Get Car</Button>
+    </>
+  )
+}
+
+export default App
